564852d3bbfcdcd57b22e74b4d42f65788892b92
ADRIAN ROSAS <adrianrosley@gmail.com>
2025-06-01 19:07:39 -0700

    jac_geometrico corregido

--- Rx.m
+++
@@ -1,4 +0,0 @@
-function R = Rx(theta)
-%Rx realiza una rotación en un ángulo theta en radianes con respecto al eje X
-
-R = [1,0,0;0, cos(theta), -sin(theta);0, sin(theta), cos(theta)];
--- Rz.m
+++
@@ -1,4 +0,0 @@
-function R = Rz(theta)
-%Rz realiza una rotación en un ángulo theta en radianes con respecto al eje z
-
-R = [cos(theta), -sin(theta), 0; sin(theta), cos(theta),  0;0, 0 , 1];
